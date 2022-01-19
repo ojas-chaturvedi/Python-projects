d201a89d0f245b20a513de1bd304fdf7c653838b
OJ <78132391+ProfessorOhas@users.noreply.github.com>
2022-01-19 21:38:02 +0000

    Optimized brick layer

--- Brick Layer/main.py
+++ Brick Layer/main.py
@@ -1,60 +1,61 @@
+#!/usr/bin/env python3
+
 import turtle
-screen = turtle.Screen()
-screen.screensize(400, 400)
-screen.setworldcoordinates(0, 0, 400, 400)
-drawer = turtle.Turtle()
-drawer.color("red")
-drawer.up()
-drawer.hideturtle()
-drawer.speed(0)
-def drawRectangle(x, y, length, height):
-    drawer.up()
-    drawer.goto(x,y)
-    drawer.begin_fill()
-    drawer.down()
-    drawer.goto(x + length, y)
-    drawer.goto(x + length, y + height)
-    drawer.goto(x, y + height)
-    drawer.goto(x, y)
-    drawer.up()
-    drawer.end_fill()
-def drawRectangleInverse(x, y, length, height):
-    drawer.up()
-    drawer.goto(x,y)
-    drawer.begin_fill()
-    drawer.down()
-    drawer.goto(x + height+length, y)
-    drawer.goto(x + length+height, y + height-length)
-    drawer.goto(x, y+length+height)
-    drawer.goto(x, y)
-    drawer.up()
-    drawer.end_fill()
-def drawCoolWall(rows, cols, brickwidth, brickHeight, mortarWidth):
-    y = brickHeight
-    brickWidth_offset = brickWidth+(mortarWidth/2)-(0.5*brickWidth) - mortarWidth
-    for s in range(rows):
-        if s%2==0:
-            x = 0
-            for i in range(cols):
-                drawRectangleInverse(x, y, brickWidth, brickHeight)
-                x += (brickWidth + mortarWidth)
-        else:
-            x = brickWidth+(mortarWidth/2)-(0.5*brickWidth)
-            for i in range(cols):
-                if i==cols-1:
-                    drawRectangleInverse(x, y, brickWidth_offset, brickHeight)
-                else:
-                    drawRectangleInverse(x, y, brickWidth, brickHeight)
-                    x += (brickWidth + mortarWidth)
-            x=0
-            drawRectangleInverse(x, y, brickWidth_offset, brickHeight)
-        y += (brickHeight + mortarWidth) + brickHeight
-    pass
+import time as t
+
+window = turtle.Screen()
+window.screensize(400, 400)
+window.setworldcoordinates(0, 0, 400, 400)
+
+turtle = turtle.Turtle()
+turtle.color("red")
+turtle.up()
+turtle.hideturtle()
+turtle.speed(0)
+
+
+def drawRectangle(x, y, width, height):
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.begin_fill()
+    turtle.down()
+    turtle.goto(x + width, y)
+    turtle.goto(x + width, y + height)
+    turtle.goto(x, y + height)
+    turtle.goto(x, y)
+    turtle.up()
+    turtle.end_fill()
+
+
+def drawRectangleInverse(x, y, width, height):
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.begin_fill()
+    turtle.down()
+    turtle.goto(x + height+width, y)
+    turtle.goto(x + width+height, y + height-width)
+    turtle.goto(x, y+width+height)
+    turtle.goto(x, y)
+    turtle.up()
+    turtle.end_fill()
+
+
+def drawBrickWall(rows, cols, brickWidth, brickHeight, mortarWidth):
+    y = 0
+    for _ in range(rows):
+        x = 0
+        for _ in range(cols):
+            drawRectangle(x, y, brickWidth, brickHeight)
+            x += (brickWidth + mortarWidth)
+        y += (brickHeight + mortarWidth)
+
+
 def drawBrickWallOffset(rows, cols, brickWidth, brickHeight, mortarWidth):
     y = 0
-    brickWidth_offset = brickWidth+(mortarWidth/2)-(0.5*brickWidth) - mortarWidth
+    brickWidth_offset = brickWidth + \
+        (mortarWidth/2)-(0.5*brickWidth) - mortarWidth
     for s in range(rows):
-        if s%2==0:
+        if s % 2 == 0:
             x = 0
             for i in range(cols):
                 drawRectangle(x, y, brickWidth, brickHeight)
@@ -62,28 +63,58 @@ def drawBrickWallOffset(rows, cols, brickWidth, brickHeight, mortarWidth):
         else:
             x = brickWidth+(mortarWidth/2)-(0.5*brickWidth)
             for i in range(cols):
-                if i==cols-1:
-                    drawRectangle(x, y, brickWidth_offset, brickHeight)     
+                if i == cols-1:
+                    drawRectangle(x, y, brickWidth_offset, brickHeight)
                 else:
                     drawRectangle(x, y, brickWidth, brickHeight)
                     x += (brickWidth + mortarWidth)
-            x=0
+            x = 0
             drawRectangle(x, y, brickWidth_offset, brickHeight)
         y += (brickHeight + mortarWidth)
-    pass
-def drawBrickWall(rows, cols, brickWidth, brickHeight, mortarWidth):
-    y = 0
+
+
+def drawCoolWall(rows, cols, brickWidth, brickHeight, mortarWidth):
+    y = brickHeight
+    brickWidth_offset = brickWidth + \
+        (mortarWidth/2)-(0.5*brickWidth) - mortarWidth
     for s in range(rows):
-        x = 0
-        for i in range(cols):
-            drawRectangle(x, y, brickWidth, brickHeight)
-            x += (brickWidth + mortarWidth)
-        y += (brickHeight + mortarWidth)
-    pass
-rows = int(input('Insert the integer amount of rows: '))
-cols = int(input('Insert the integer amount of columns: '))
-brickWidth = int(input('Insert the integer amount of the width of the bricks: '))
-brickHeight = int(input('Insert the integer amount of the height of the bricks: '))
-mortarWidth = int(input('Insert the integer amount of how much space you want between the bricks: '))
-drawBrickWallOffset(rows,cols,brickWidth,brickHeight,mortarWidth)
-screen.exitonclick()
+        if s % 2 == 0:
+            x = 0
+            for i in range(cols):
+                drawRectangleInverse(x, y, brickWidth, brickHeight)
+                x += (brickWidth + mortarWidth)
+        else:
+            x = brickWidth+(mortarWidth/2)-(0.5*brickWidth)
+            for i in range(cols):
+                if i == cols-1:
+                    drawRectangleInverse(x, y, brickWidth_offset, brickHeight)
+                else:
+                    drawRectangleInverse(x, y, brickWidth, brickHeight)
+                    x += (brickWidth + mortarWidth)
+            x = 0
+            drawRectangleInverse(x, y, brickWidth_offset, brickHeight)
+        y += (brickHeight + mortarWidth) + brickHeight
+
+
+def main():
+    rows = int(input('Insert the amount of rows: '))
+    cols = int(input('Insert the integer amount of columns: '))
+    brickWidth = int(
+        input('Insert the integer amount of the width of the bricks: '))
+    brickHeight = int(
+        input('Insert the integer amount of the height of the bricks: '))
+    mortarWidth = int(input(
+        'Insert the integer amount of how much space you want between the bricks: '))
+    drawBrickWall(rows, cols, brickWidth, brickHeight, mortarWidth)
+    t.sleep(5)
+    window.clear()
+    drawBrickWallOffset(rows, cols, brickWidth, brickHeight, mortarWidth)
+    t.sleep(5)
+    window.clear()
+    drawCoolWall(rows, cols, brickWidth, brickHeight, mortarWidth)
+    window.exitonclick()
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
